105a75f105de020e22cf0f1538b7d3689e72072a
Symfomany <zuzu38080@gmail.com>
2018-04-05 09:02:41 +0200

    add changes

---
+++ main.js
@@ -0,0 +1,9 @@
+/**
+ * Take a sentence to return a Verlan language
+ * @param {String} phrase
+ */
+function verlan(phrase) {
+  return "";
+}
+
+verlan("Les maisons près de la fontaine dans le quartier");
